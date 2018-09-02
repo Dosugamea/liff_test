5400ce86db9c0f203572f55ed41962dd8ec7cbf1
Dosugamea@SourceTree <17107514+Dosugamea@users.noreply.github.com>
2018-09-02 23:56:31 +0900

    Update

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,8 @@
 window.onload = function (e) {
     liff.init((data)=>{
 		var urlParams = new URLSearchParams(window.location.search);
+		alert(urlParams.get('flex'));
+		alert(JSON.parse(urlParams.get('flex')));
 		liff.sendMessages([
 			{
 				type:'flex',
